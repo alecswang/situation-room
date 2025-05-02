52aa1f7eb5f22e9e1461cffb6128f0c716c3b0f4
alexwang <alecs_wang@berkeley.edu>
2025-05-02 11:31:17 -0700

    adjusted title and favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Rubber Room',
+  title: 'Situation Room',
   description: 'A platform for activists and journalists to connect',
+  icons: {
+    icon: '/favicon.ico',
+  },
 };
 
 export default function RootLayout({
@@ -16,6 +20,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body className={`${inter.className} bg-gray-50`}>
         {children}
       </body>
